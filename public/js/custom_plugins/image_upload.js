173c8f6cbd691152a81bc955fddbbfa2993c65bd
Ryan Kamal <ryaankamal@gmail.com>
2017-11-12 10:55:16 +0200

    تعديلات متنوعة

--- public/js/custom_plugins/image_upload.js
+++ public/js/custom_plugins/image_upload.js
@@ -6,9 +6,10 @@ $(function(){
       height: '',
       background: '',
       border: '',
+      radius: '',
     },options);
     var form = '<div class="image_upload"> <input type="file" id="'+settings.name+'" name="'+settings.name+'" hidden="" /> <label for="'+settings.name+'"> <i class="ft-camera"></i> </label></div>';
     $(this).append(form);
-    $(this).find('.image_upload label').css('width',settings.width).css('height',settings.height).css('background',settings.background).css('border-color',settings.border);
+    $(this).find('.image_upload label').css('width',settings.width).css('height',settings.height).css('background',settings.background).css('border-color',settings.border).css('border-radius',settings.radius);
   }
 }( jQuery ));
