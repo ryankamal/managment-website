83a455590ca6e3262c7e3ae20131415dafb8cc9f
Ryan Kamal <ryaankamal@gmail.com>
2017-11-26 09:53:36 +0200

    تعديلات صفحة المخزن

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -1,10 +1,31 @@
 exports.install = function() {
     F.route('/inventory', '/inventory/inventory');
     F.route('/recieve_permission', recieve_permission);
+    F.route('/export_permission', export_permission);
+    F.route('/destroy_permission', destroy_permission);
+    F.route('/return_permission', return_permission);
 };
 
 function recieve_permission(){
     var self = this;
     self.layout('');
     self.view('/inventory/recieve_permission');
+}
+
+function export_permission(){
+    var self = this;
+    self.layout('');
+    self.view('/inventory/export_permission');
+}
+
+function destroy_permission(){
+    var self = this;
+    self.layout('');
+    self.view('/inventory/destroy_permission');
+}
+
+function return_permission(){
+    var self = this;
+    self.layout('');
+    self.view('/inventory/return_permission');
 }
