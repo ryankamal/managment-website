4e875c8f6b5e7887804e459843c9929139a0a745
Ryan Kamal <ryaankamal@gmail.com>
2017-11-29 03:35:46 +0200

    seller fix

--- models/inventories.js
+++ models/inventories.js
@@ -5,10 +5,10 @@ NEWSCHEMA('Inventories').make(function(schema) {
             if(options.order == 'preview_inventory'){
                 /** for example /inventories/2 handling */
                 //sql.debug = true;
-                sql.query('inventory_data',"select * from inventories where id ="+options.id+" ");
+                sql.query('inventory_data',"select inventories.* , sum(inventories_items.amount::float * inventories_items.price::float) as i_total_value , count(inventories_items.*) as i_total_items, avg(inventories_items.amount::float * inventories_items.price::float) as i_avg_value from inventories,inventories_items where inventories.id = "+options.id+" and inventories.id = inventories_items.i_id group by inventories.id, inventories.i_name,inventories.i_branch,inventories.i_desc,inventories.i_address,inventories.i_color,inventories.i_mobile");
                 sql.query('inventory_items',"select * from inventories_items where i_id = "+options.id+"");
                 sql.query('inventory_expires',"select * , expiry - NOW() as still from inventories_items where expiry <= NOW() + '5 DAYS'::INTERVAL and i_id = "+options.id+" ");
-                sql.query('inventory_latest',"select inventories_items.* , inventory_permissions.type from inventories_items, inventory_permissions where inventories_items.p_id = inventory_permissions.id");
+                sql.query('inventory_latest',"select inventories_items.* , inventory_permissions.type from inventories_items, inventory_permissions where inventories_items.p_id = inventory_permissions.id union select inventory_permissions_items.* , inventory_permissions.type from inventory_permissions_items, inventory_permissions where inventory_permissions_items.p_id = inventory_permissions.id");
                 sql.exec(function(err,response){
                     console.log('inventory_data',response.inventory_data);
                     console.log('inventory_items',response.inventory_items);
